02639e5ad0b4ffa23e8ed3ba73ae5831ca06db13
Kade Hess <kadeahess@gmail.com>
2022-08-30 14:53:08 -0700

    functions lab peer

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+    return Math.abs(distance-42)
+}
+
+function distanceFromHqInFeet(distance) {
+    return Math.abs(distance-42) * 264
+}
